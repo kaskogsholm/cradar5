a29db23eb9babe8a825ded00f2e19c2f81d8d214
Karsten Skogsholm <kskogsholm@intralytix.com>
2024-10-13 17:57:41 -0400

    Started Paul example. Got confuesed. Quit.

--- src/DelayDifferentialSystem.h
+++ src/DelayDifferentialSystem.h
@@ -2,19 +2,21 @@
 #include <Eigen/Eigen>
 #pragma once
 
-template <typename System>
+template <typename System, int dimension>
 concept hasDerivative = requires(System sys, double time) {
   {
     sys.derivative(time)
-  } -> std::convertible_to<Eigen::Ref<const Eigen::VectorX<double>>>;
-  { sys.derivative(time).cols() } -> std::convertible_to<int>;
-  { sys.derivative(time).rows() } -> std::convertible_to<int>;
+  } -> std::convertible_to<Eigen::Ref<const Eigen::Vector<double, dimension>>>;
+  /*{ sys.derivative(time).cols() } -> std::convertible_to<int>;*/
+  /*{ sys.derivative(time).rows() } -> std::convertible_to<int>;*/
 };
 
-template <typename ChildSystem> class DelayDifferentialSystem {
+template <typename ChildSystem, int dimension> class DelayDifferentialSystem {
 public:
   DelayDifferentialSystem() {
-    static_assert(hasDerivative<ChildSystem> &&
-                  std::derived_from<ChildSystem, DelayDifferentialSystem>);
+    static_assert(hasDerivative<ChildSystem, dimension> &&
+                      std::derived_from<ChildSystem, DelayDifferentialSystem>,
+                  "Delay differential equation did not satisfy the "
+                  "'hasDerivative' concept.");
   }
 };
--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,28 @@
 #include "DelayDifferentialSystem.h"
 #include <Eigen/Eigen>
-class systemEx : public DelayDifferentialSystem<systemEx> {
+class Paul : public DelayDifferentialSystem<Paul, 1> {
 public:
-  Eigen::Vector3d derivative(double t) {
-    Eigen::Array3<int> res;
-    res << 1, 2, 3;
+  using Vector = Eigen::Vector<double, 1>;
+  Eigen::Vector<double, 1> derivative(
+      double t, const Eigen::Ref<const Vector> &parameters,
+      const Eigen::Ref<const Vector> &state_vector,
+      const Eigen::Ref<const Eigen::Vector<double, 1>> &delayed_state_vector) {
+    Eigen::Vector<double, 1> res;
+    res = delayed_state_vector;
     return res;
   }
+
+  double delay(double t,
+               const Eigen::Ref<const Eigen::Vector<double, 2>> &parameters,
+               const Eigen::Ref<const Eigen::Vector<double, 1>> &state_vector) {
+    return state_vector(0);
+  }
+
+  // Need Jacobian
+  // Need delayed Jac
+  // Need Phi, history func
 };
-int main() { DelayDifferentialSystem<systemEx> mysys; }
+int main() {
+  Paul paul_system;
+  /*paul_system.solve(init, start_time, [0.1])*/
+}
